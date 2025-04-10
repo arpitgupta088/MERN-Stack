4aeb4c2580a8ede102b5716947304a2f88ee89ce
arpit gupta <arpitgupta14786gmail.com>
2025-04-10 16:40:54 +0530

    ejs required

---
+++ EJSdir/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+const port = 8080;
+
+app.set("view engine", "ejs");
+// wo package jo hamare templates ko create krne ke kaam ayga wo hoga ejs
+
+app.set("views", path.join(__dirname, "/views"))
+// views ke path ko add kr dena h is constant path (i.e. ejsdir) ke saath
+// taaki ham agr bahar se bhi server ko run krrhe hai to error na aye
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");  //express apne aap views directory k andar jakar check krega
+});
+app.get("/hello", (req, res) => {
+    res.send("hello");
+});
+
+app.get("/rolldice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", {num: diceVal});
+});
+
+app.listen(port, () => {
+    console.log(`listning on port ${port}`);
+});
