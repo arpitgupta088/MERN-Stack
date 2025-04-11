99907b74a4364a4c55adb81cd9fe09e1787c6246
arpit gupta <arpitgupta14786gmail.com>
2025-04-12 00:39:23 +0530

    added insta exmple

--- EJSdir/index.js
+++ EJSdir/index.js
@@ -18,6 +18,20 @@ app.get("/hello", (req, res) => {
     res.send("hello");
 });
 
+app.get("/ig/:username", (req, res) => {
+    const followers = ["adam", "eve", "steve"];
+    let {username} = req.params;
+    res.render("instagram.ejs", {username, followers});
+});
+
+app.get("/ig2/:username", (req, res) => {
+    let {username} = req.params;
+    const instaData = require("./data.json");   
+    const data = instaData[username]; 
+    console.log(data);
+    res.render("instagram2.ejs", {data});
+});
+
 app.get("/rolldice", (req, res) => {
     let diceVal = Math.floor(Math.random() * 6) + 1;
     res.render("rolldice.ejs", {num: diceVal});
